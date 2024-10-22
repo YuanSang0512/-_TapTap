8c183affd49d175ec350d88f634a04eb4ba9bda2
onlySYstem0331 <1935085776@qq.com>
2024-10-22 14:53:44 +0800

    实现屏幕截图

--- Assets/Scenes/ScreenShooting/Scene.cs
+++ Assets/Scenes/ScreenShooting/Scene.cs
@@ -1,19 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Scene : MonoBehaviour
 {
     public Camera cameraToCapture; // 指定要捕获的相机
-    public string savePath = "Screenshot.png"; // 保存路径
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F12))
-        {
-            CaptureAndSave();
-        }
-    }
+    public string savePath = "AssetsResources/Screenshots/Screenshot.png"; // 保存路径
+    public SpriteRenderer spriteRenderer;          //截屏展示的画板
+    [SerializeField]GameObject UI_toshow;                  //使用照相功能对应的UI; 
+    [SerializeField]GameObject UI_Image;                  //使用照相功能对应的图片; 
+        
 
     public void CaptureAndSave()
     {
@@ -27,21 +24,59 @@ public class Scene : MonoBehaviour
         // 创建Texture2D以保存图像
         Texture2D texture2D = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
 
-        // 读取渲染纹理到Texture2D
         RenderTexture.active = renderTexture;
         texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
         texture2D.Apply();
 
-        // 重置相机目标纹理
         cameraToCapture.targetTexture = null;
-        RenderTexture.active = null; // 注意释放 RenderTexture
+        RenderTexture.active = null; 
 
         // 保存Texture2D为PNG文件
         byte[] bytes = texture2D.EncodeToPNG();
         System.IO.File.WriteAllBytes(savePath, bytes);
 
-        // 清理Texture2D
-        Destroy(texture2D);
+        Texture2D texture2D1 = LoadTexture(savePath);
+
+        if (texture2D1 != null)
+        {
+            // 创建Sprite
+            Sprite sprite = Sprite.Create(texture2D1, new Rect(0.0f, 0.0f, texture2D1.width, texture2D1.height), new Vector2(0.5f, 0.5f), 100.0f);
+            spriteRenderer.sprite = sprite;
+
+
+            UI_toshow.SetActive(true);
+            UI_Image.SetActive(true);
+
+
+            // 清理Texture2D
+            Destroy(texture2D);
+        }
     }
+
+
+
+    public Texture2D LoadTexture(string filePath)      
+    {
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("File not found: " + filePath);
+            return null;
+        }
+
+        // 读取文件数据
+        byte[] fileData = File.ReadAllBytes(filePath);
+        // 创建Texture2D并加载图片数据
+        Texture2D texture = new Texture2D(2, 2);
+        bool isLoaded = texture.LoadImage(fileData);
+
+        if (!isLoaded)
+        {
+            Debug.LogError("Failed to load image data from " + filePath);
+            return null;
+        }
+
+        return texture;
+    }
+
 }
 
