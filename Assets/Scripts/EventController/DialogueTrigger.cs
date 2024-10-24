19ac154085b9a4a3d6c3156db17402ff05585947
onlySYstem0331 <1935085776@qq.com>
2024-10-24 20:42:53 +0800

    剧情对话10-24版本

--- Assets/Scripts/EventController/DialogueTrigger.cs
+++ Assets/Scripts/EventController/DialogueTrigger.cs
@@ -5,16 +5,21 @@ using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 public class DialogueTrigger : MonoBehaviour, IEventTrigger
 {
+    [SerializeField] int dialog;
+    [SerializeField]rollcharacter rollcharacter;
+    [SerializeField]GameObject UI;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            UI.SetActive(true);
+
             Event();
         }
     }
 
     public void Event()
     {
-        
+        rollcharacter.Dialog_index = dialog;
     }
 }
--- Assets/Scripts/UI/rollcharacter.cs
+++ Assets/Scripts/UI/rollcharacter.cs
@@ -7,11 +7,13 @@ public class rollcharacter : MonoBehaviour
 {
     [SerializeField] bool IfFinish = false;
     [SerializeField] bool IfJump = false;
+    public int Dialog_index;
 
     public TextAsset peibiao;
     public TMP_Text textComponent;
     public float delayBetweenChars = 0.1f;
-    public string[] dialog;
+    public string[] dialog_All;
+    public string[][] dialog_Tail;
     public int index;
 
     private string TempString = "";
@@ -28,8 +30,15 @@ public class rollcharacter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        dialog = peibiao.text.Split('\n');
-        // 将List转换为Dictionary
+        dialog_All = peibiao.text.Split('#');
+
+
+        dialog_Tail = new string[dialog_All.Length][];
+        for (int i = 0; i < dialog_All.Length; i++)
+        {
+            // 使用 Split 方法分割每个对话字符串，并将结果赋给 dialog_Tail 的相应位置
+            dialog_Tail[i] = dialog_All[i].Split('\n');
+        }
         CharacterName_Sprites = new Dictionary<string, Sprite>();
         for (int i = 0; i < characterNames.Count; i++)
         {
@@ -42,54 +51,53 @@ public class rollcharacter : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (IfFinish)
-            {
-                index++;
-                if (index >= dialog.Length - 1)
-                {
-                    index = 0;
-                    GameStory.SetActive(false);
-                }
-                StartCoroutine(ShowText());
-            }
-            else if (!IfJump && !IfFinish)
-            {
-                IfJump = true;
-            }
-        }
+        Talking_Begin(Dialog_index);
     }
 
-    IEnumerator ShowText()
+    IEnumerator ShowText(int i)
     {
-        textComponent.color = Color.red;
         IfFinish = false;
         textComponent.text = TempString;
         int letter = 0;
 
         // 新增：从对话中提取角色名，假设对话格式为 "角色名: 对话内容"
-        string characterName = dialog[index].Split('：')[0].Trim();
+        string characterName = dialog_Tail[i][index].Split('：')[0].Trim();
         // 显示对应的角色图片
         if (CharacterName_Sprites.ContainsKey(characterName))
         {
             characterSpriteRenderer.sprite = CharacterName_Sprites[characterName];
         }
 
-        while (!IfJump && letter < dialog[index].Length)
+        while (!IfJump && letter < dialog_Tail[i][index].Length)
         {
-            textComponent.text += dialog[index][letter];
+            textComponent.text += dialog_Tail[i][index][letter];
             letter++;
             yield return new WaitForSeconds(delayBetweenChars);
         }
         IfJump = false;
-        textComponent.text = dialog[index].Substring(characterName.Length + 1).Trim(); // 显示对话内容，去掉角色名
+        textComponent.text = dialog_Tail[i][index].Substring(characterName.Length + 1).Trim(); // 显示对话内容，去掉角色名
         IfFinish = true;
     }
 
-    public void NpcStart()
+
+    public void Talking_Begin(int i)    
     {
-        GameStory.SetActive(true);
-        StartCoroutine(ShowText());
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (IfFinish)
+            {
+                index++;
+                if (index >= dialog_Tail[i].Length - 1)
+                {
+                    index = 0;
+                    GameStory.SetActive(false);
+                }
+                StartCoroutine(ShowText(i));
+            }
+            else if (!IfJump && !IfFinish)
+            {
+                IfJump = true;
+            }
+        }
     }
 }
